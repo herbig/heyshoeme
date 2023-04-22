36b69f97fce26ecd7310ec0c08caec7751f11e0c
herbig <mikeherbig@gmail.com>
2023-04-22 15:43:39 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,83 @@
-import * as React from "react"
 import {
   ChakraProvider,
-  Box,
   Text,
   Link,
   VStack,
+  Image,
   Code,
-  Grid,
-  theme,
+  Center
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { Route, Routes, useLocation } from "react-router-dom";
+import web3 from "web3";
+import { BigNumber } from 'ethers';
+import { extendTheme } from '@chakra-ui/react'
+
+const themeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const COLLECTION_URL = 'https://rarible.com/token/polygon/0xd67d0dd4b6e8639f8c51f60bfcb646cb6ed5e993';
+const CONTRACT_URL = 'https://polygonscan.com/address/TODOTODO#code';
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+  <ChakraProvider theme={extendTheme({ themeConfig })}>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/*' element={<Redirect />} />
+    </Routes>
   </ChakraProvider>
 )
+
+function Redirect() {
+
+  const path: string = useLocation().pathname;
+
+  let username: string = path.substring(1, path.length).toLowerCase();
+
+  if (username.startsWith('@')) {
+    username = username.substring(1, path.length);
+  }
+
+  if (username.length < 4 || username.length > 15 || !/^[_a-z0-9]*$/.test(username)) {
+    return (<>{window.location.replace('/')}</>);
+  }
+
+  const id: BigNumber = BigNumber.from(web3.utils.soliditySha3(username));
+
+  return (<>{window.location.replace(COLLECTION_URL + ':' + id.toString())}</>);
+}
+
+function Home() {
+  return (
+      <Center padding={8}>
+          <VStack spacing={8} width="50%" fontSize="xl">
+            <Image src={process.env.PUBLIC_URL + '/logo192.png'} />
+            <Text>
+              <i>ShoeMe</i> is an NFT-based Aspirationally Decentralized self-sovereign identity system, 
+              contingent on placing a shoe on top of your head.
+            </Text>
+            <Text>
+              To get verified on <i>ShoeMe</i>, simply post a photo of your real self with a shoe on your head, 
+              along with the hashtag <i>#heyshoeme</i>, and our AI system will do the rest.
+            </Text>
+            <Text>
+              Once verified, you may place a link to your verification NFT in your profile in the form &#20; 
+              <Code>heyshoe.me/&#123;your username&#125;</Code>. Please refresh this page continually, as we
+              will not notify you when it is ready.
+            </Text>
+            <Text>
+              Please allow up to 2 days to process, as our AI takes frequent breaks.
+            </Text>
+            <Link
+              color="teal.500"
+              href={CONTRACT_URL}
+              fontSize="2xl"
+              target="_blank"
+            >
+              smart contract
+            </Link>
+          </VStack>
+      </Center>
+  );
+}
