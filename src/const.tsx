ff8fe308306cff0c8a5ab7b42b9d0e8e6c6fa635
herbig <mikeherbig@gmail.com>
2023-04-23 19:38:42 -0400

    update contracts

--- src/const.tsx
+++ src/const.tsx
@@ -1,4 +1,4 @@
-export const COLLECTION_ADDRESS = '0x1a2fda0b1f4d3c201b42c5fe58091e9739857b95';
+export const COLLECTION_ADDRESS = '0xbd05e413f7f581d7d2a7d8dadadfdebfb8f65b85';
 export const COLLECTION_URL = `https://rarible.com/token/polygon/${COLLECTION_ADDRESS}`;
 export const CONTRACT_URL = `https://polygonscan.com/address/${COLLECTION_ADDRESS}#code`;
 export const PAD_MD = 8;
