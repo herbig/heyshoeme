a6034339127cd7f1220494505377bb9f6a910284
herbig <mikeherbig@gmail.com>
2023-04-23 18:54:15 -0400

    blob it instead of using URL, so the html2canvas works

--- src/ui/VerifiedImage.tsx
+++ src/ui/VerifiedImage.tsx
@@ -1,5 +1,6 @@
 import { HTMLChakraProps, Image, Box } from "@chakra-ui/react";
 import mask from "../images/shoe_mask.png"
+import { useEffect, useState } from "react";
 
 interface Props extends Partial<HTMLChakraProps<"div">> {
     imageRef?: React.MutableRefObject<HTMLElement>;
@@ -7,10 +8,30 @@ interface Props extends Partial<HTMLChakraProps<"div">> {
 }
   
 export default function VerifiedImage({ imageRef, imageUrl, ...rest }: Props) {
-    return (
-      <Box ref={imageRef} width="400px" height="400px" {...rest}>
-        <Image src={imageUrl} position={"absolute"}/>
-        <Image src={mask} position={"relative"}/>
-      </Box>
-    );
+
+  const [blob, setBlob] = useState<Blob>();
+
+  useEffect(() => {
+    async function fetchData() {
+      if (imageUrl) {
+        const imgBlob = await fetch(imageUrl).then(r => r.blob());
+        setBlob(imgBlob);
+      }
+    }
+    fetchData();
+  }, [imageUrl]);
+
+  let src: string | undefined;
+  if (blob) {
+    src = URL.createObjectURL(blob);
+  } else {
+    src = undefined;
+  }
+  
+  return (
+    <Box ref={imageRef} width="400px" height="400px" {...rest}>
+      <Image src={src} position={"absolute"}/>
+      <Image src={mask} position={"relative"}/>
+    </Box>
+  );
 }
