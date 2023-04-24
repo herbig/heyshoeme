dda0ad44b3d5f3a3d0cd12e04fb27f14c41a6e7f
herbig <mikeherbig@gmail.com>
2023-04-24 17:29:00 -0400

    updated copy

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -6,21 +6,25 @@ export default function Home() {
       <Center padding={PAD_MD}>
         <VStack spacing={PAD_MD} width={["90%", "70%", "70%", "50%"]} fontSize="xl">
           <Image src={process.env.PUBLIC_URL + '/logo192.png'} />
+
           <Text>
-            <i>ShoeMe</i> is an NFT-based Aspirationally Decentralized self-sovereign identity system, 
-            contingent on placing a shoe on top of your head.
+          Have you ever been impersonated by a bad actor on social media? Ever gotten asked "R U a bot?" on social media?
           </Text>
           <Text>
-            To get verified on <i>ShoeMe</i>, simply post a photo of your real self with a shoe on your head, 
-            along with the hashtag <i>#heyshoeme</i>, and our AI system will do the rest.
+          Enter ShoeMe, a Proof of Identity platform that aspires to be entirely owned by the people, for the people. Our mission is to create a world where people shouldn't have to pay to be considered people. 
           </Text>
           <Text>
-            Once verified, you may place a link to your verification NFT in your profile in the form &#20; 
-            <Code>heyshoe.me/&#123;your username&#125;</Code>. Please refresh this page continually, as we
-            will not notify you when it is ready.
+          Ready to fully realize your digital personhood? Follow these 2 easy steps to get verified:
           </Text>
           <Text>
-            Please allow up to 2 days to process, as our AI takes frequent breaks.
+          👉 Tweet a photo of your real (carbon-based) self with a shoe on your head. Make sure to include the hashtag #heyshoeme in your Tweet. (This can be any shoe you own or borrow, but please do not steal or use a shoe without its owner's explicit permission).
+          </Text>
+          <Text>👉 Sit back, relax, and wait to become a verified digital person. Our highly-complex AI will do the rest.</Text>
+          <Text>Once your verification is processed, your ShoeMe personhood support representative will reply to your Tweet with a link to your Digital Personhood certification. That link will take you to an unalterable and immutable version of your Twitter profile picture with the official ShoeMe Stamp of Digital Personhood™.</Text>
+          <Text>Now you're ready to share your fully-verified identity with the world! 🎉</Text>
+          <Text>Use your new Digital Personhood profile pic across all your social platforms + make sure to include the link to your certification in your bio.</Text>
+          <Text>
+          Don't forget to share ShoeMe with all your carbon-based friends and loved ones so they can become fully realized digital people too.
           </Text>
           <Link
             color="teal.500"
